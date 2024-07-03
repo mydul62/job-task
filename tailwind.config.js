64c8a112f8bf160f7b298338668590daa8d37796
unknown <mydulcse62.niter@gmail.com>
2024-07-03 20:54:01 +0600

    job task project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'program': "url('..images')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  }
+}
